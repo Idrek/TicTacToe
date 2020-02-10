7b1a60449480a31f9155632a071eed9839aaca47
Idrek <iddarg@gmail.com>
2020-02-10 16:06:18 +0100

    Test `isWinner` function

--- test/Tests.fs
+++ test/Tests.fs
@@ -1,8 +1,59 @@
 module Tests
 
-open System
+open TicTacToe.App
 open Xunit
 
+// ---------------------------------
+// isWinner
+// ---------------------------------
+
 [<Fact>]
-let ``My test`` () =
-    Assert.True(true)
+let ``Test of isWinner function`` () =
+    let initialValue : char = ' '
+    let board : char[,] = 
+        [|
+            [|'x';'x';'o'|]
+            [|'x';'x';'o'|]
+            [|' ';' ';' '|]
+        |] |> array2D
+    Assert.Equal(false, isWinner initialValue board)
+
+    let board : char[,] = 
+        [|
+            [|' ';' ';' '|]
+            [|' ';' ';' '|]
+            [|' ';' ';' '|]
+        |] |> array2D
+    Assert.Equal(false, isWinner initialValue board)
+
+    let board : char[,] = 
+        [|
+            [|'x';' ';' '|]
+            [|' ';'x';' '|]
+            [|' ';' ';'x'|]
+        |] |> array2D
+    Assert.Equal(true, isWinner initialValue board)
+
+    let board : char[,] = 
+        [|
+            [|'o';'o';'o'|]
+            [|' ';'x';' '|]
+            [|' ';' ';'x'|]
+        |] |> array2D
+    Assert.Equal(true, isWinner initialValue board)
+
+    let board : char[,] = 
+        [|
+            [|'x';' ';'o'|]
+            [|' ';'x';'o'|]
+            [|' ';' ';'o'|]
+        |] |> array2D
+    Assert.Equal(true, isWinner initialValue board)
+
+    let board : char[,] = 
+        [|
+            [|'x';'o';'x'|]
+            [|' ';'x';'o'|]
+            [|'x';' ';'o'|]
+        |] |> array2D
+    Assert.Equal(true, isWinner initialValue board)
